70a618b6c452fe0199940c74dacd1a11c94ce780
sjindal-01 <sadhil.jindal@gmail.com>
2021-03-26 23:05:46 +0000

    Test

--- src/main/java/shield/Main.java
+++ src/main/java/shield/Main.java
@@ -5,9 +5,13 @@
 
 package shield;
 
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("running!");
+    String response = ClientIO.doGETRequest("http://127.0.0.1:5000/showFoodBox?orderOption=catering&dietaryPreference=none");
+    System.out.println(response);
     return;
   }
 }
