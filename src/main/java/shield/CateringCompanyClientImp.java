ea3d317a147e40f85d0f5474345497ddbc084314
sjindal-01 <sadhil.jindal@gmail.com>
2021-03-27 15:47:10 +0000

    Added endpoint field

--- src/main/java/shield/CateringCompanyClientImp.java
+++ src/main/java/shield/CateringCompanyClientImp.java
@@ -5,13 +5,15 @@
 package shield;
 
 public class CateringCompanyClientImp implements CateringCompanyClient {
+
+  String endpoint;
+
   public CateringCompanyClientImp(String endpoint) {
+    this.endpoint = endpoint;
   }
 
   @Override
-  public boolean registerCateringCompany(String name, String postCode) {
-    return false;
-  }
+  public boolean registerCateringCompany(String name, String postCode) { return false; }
 
   @Override
   public boolean updateOrderStatus(int orderNumber, String status) {
